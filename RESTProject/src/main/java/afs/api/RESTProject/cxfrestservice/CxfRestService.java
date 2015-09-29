e981ff80289f9a0b4a16f30792f3e62e192d2bd7
Matt Green <matthew.a.green@accenturefederal.com>
2015-09-28 23:28:43 -0400

    Working REST Project

--- RESTProject/src/main/java/afs/api/RESTProject/cxfrestservice/CxfRestService.java
+++ RESTProject/src/main/java/afs/api/RESTProject/cxfrestservice/CxfRestService.java
@@ -1,5 +1,41 @@
 package afs.api.RESTProject.cxfrestservice;
 
-public interface CxfRestService {
+import java.util.ArrayList;
 
+import javax.jws.WebService;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.springframework.http.MediaType;
+
+import afs.api.RESTProject.cxfrestservice.model.Award;
+
+
+@Path("/")
+@WebService(name="awardService")
+public interface CxfRestService {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON_VALUE)
+	@Path("/")
+	public ArrayList<Award> getAllAwards();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON_VALUE)
+	@Path("/{awardId}")
+	public Award getAward(Long awardId);
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON_VALUE)
+	@Path("/{awardId}")
+	public Response updateAward(Long awardId);
+	
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON_VALUE)
+	@Path("/{awardId}")
+	public Response deleteAward(Long awardId);
 }
--- RESTProject/src/main/java/afs/api/RESTProject/cxfrestservice/internal/CxfRestServiceInternal.java
+++ RESTProject/src/main/java/afs/api/RESTProject/cxfrestservice/internal/CxfRestServiceInternal.java
@@ -1,5 +1,42 @@
 package afs.api.RESTProject.cxfrestservice.internal;
 
-public class CxfRestServiceInternal {
+import java.util.ArrayList;
+
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import afs.api.RESTProject.cxfrestservice.CxfRestService;
+import afs.api.RESTProject.cxfrestservice.dao.AwardDao;
+import afs.api.RESTProject.cxfrestservice.model.Award;
+
+public class CxfRestServiceInternal implements CxfRestService {
+
+	@Autowired
+	private AwardDao awardDao;
+	
+	@Override
+	public ArrayList<Award> getAllAwards() {
+		// TODO Auto-generated method stub
+		return awardDao.getAllAwards();
+	}
+
+	@Override
+	public Award getAward(Long awardId) {
+		// TODO Auto-generated method stub
+		return awardDao.getAward(awardId);
+	}
+
+	@Override
+	public Response updateAward(Long awardId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response deleteAward(Long awardId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
